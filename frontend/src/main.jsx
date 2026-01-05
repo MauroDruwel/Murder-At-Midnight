5f8c7129e11ca21a25cb512360f1b9635af89d79
davidseeker507 <dli121710@gmail.com>
2026-01-05 22:36:56 +0100

    Resolve merge conflicts and regen lockfile

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Terminal from './components/Terminal';
+import TerminalPage from './pages/main.jsx';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Terminal />
+    <TerminalPage />
   </React.StrictMode>
 );
